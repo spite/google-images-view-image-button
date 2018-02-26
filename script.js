fc8a4ee549e3d08eb74f92d77c3b744342af9d63
Jaume Sanchez <the.spite@gmail.com>
2018-02-26 23:35:42 +0000

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', () => {
+
+	const nodes = document.querySelectorAll('.irc_but_r tr');
+
+	for (const node of nodes) {
+
+		const td = document.createElement('td');
+		td.innerHTML = '<a><span>View Image</span></a>';
+		node.appendChild(td);
+
+		td.addEventListener('click', async e => {
+
+			const img = td.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector('.irc_mi');
+
+			if (img) {
+
+				const downloadBtn = document.createElement( 'a' );
+				//downloadBtn.setAttribute( 'download', img.src );
+				downloadBtn.setAttribute( 'target', '_blank' );
+				downloadBtn.setAttribute( 'href', img.src );
+				downloadBtn.click();
+
+				e.preventDefault();
+				e.stopPropagation();
+				return false;
+			}
+
+		});
+	}
+
+})
